febf110539584fc548f08948fc693cdcb5118d91
sajad-moradi <sajad.moradi2006@gmail.com>
2020-02-29 11:44:08 +0800

    Defining board_array methode (it makes a 2D board using board_string)

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,10 +1,19 @@
 class Sudoku
-    attr_accessor :board_string
-    ###     The suedocode of each line is written after the code in each line.     ###
-    def initialize(board_string)
-      @board_string = board_string
-      @board = Array.new
-      @empty_cells = Array.new
-      @blocks_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+  attr_accessor :board_string
+  ###     The suedocode of each line is written after the code in each line.     ###
+  def initialize(board_string)
+    @board_string = board_string
+    @board = Array.new
+    @empty_cells = Array.new
+    @blocks_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+  end
+
+  def board_array # The board_array function will convert the puzzle from string to a 2D array.
+    i = 0
+    9.times do
+      self.board.push(self.board_string[i..i + 8].split("").map { |num| num.to_i })
+      i += 9
     end
+    self.board
+  end
 end
