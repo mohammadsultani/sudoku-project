5e9ac95a095771b7f6441f8a18145cd1d4bc1148
Sajad Moradi <sajad.moradi2006@gmail.com>
2020-02-29 02:34:48 -0500

    Defining is_number_valid? method to call and check last 3 functions

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,8 +1,8 @@
 class Sudoku
-  attr_accessor :board_string
+  attr_accessor :board_string, :board, :empty_cells, :blocks_array
   ###     The suedocode of each line is written after the code in each line.     ###
   def initialize(board_string)
-    @board_string = board_string
+    self.board_string = board_string
     @board = Array.new
     @empty_cells = Array.new
     @blocks_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
@@ -18,37 +18,23 @@ class Sudoku
   end
 
   def find_empty_cells # In this function we find the empty positions in the puzzle.
-    @board.each_with_index { |row, row_index| row.each_with_index { |col, col_index| @empty_cells << [row_index, col_index] if col == 0 } }
-    @empty_cells
+    self.board.each_with_index { |row, row_index| row.each_with_index { |col, col_index| self.empty_cells << [row_index, col_index] if col == 0 } }
+    self.empty_cells
   end
 
   def check_row?(row, number) # It checks if that a number exist in its row.
-    !@board[row].include?(number)
+    !self.board[row].include?(number)
   end
 
   def check_col?(col, number) # It checks the column for a given number.
-    @board.each { |el| return false if el[col] == number }
+    self.board.each { |el| return false if el[col] == number }
     true
   end
 
   def check_block?(row, col, number) # Here we check if the number exist in its block.
-    row_array = @blocks_array.find { |el| el.include?(row) } # Is it in the row?
-    col_array = @blocks_array.find { |el| el.include?(col) } # Is it in the Column?
-    row_array.each { |row| col_array.each { |col| return false if @board[row][col] == number } } # How about the block it self?
+    row_array = self.blocks_array.find { |el| el.include?(row) } # Is it in the row?
+    col_array = self.blocks_array.find { |el| el.include?(col) } # Is it in the Column?
+    row_array.each { |row| col_array.each { |col| return false if self.board[row][col] == number } } # How about the block it self?
     true
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
 end
-
